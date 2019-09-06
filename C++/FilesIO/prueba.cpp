9166239d9a30fb88e7e7ed6e13a5c8ac707b8278
Elvis PTI <elvisavfc65@gmail.com>
2019-09-06 15:14:26 -0400

    added some examples

--- C++/FilesIO/prueba.cpp
+++ C++/FilesIO/prueba.cpp
@@ -1,33 +1,20 @@
 #include <iostream>
-#include <string>
+#include  <cstring>
+
 using namespace std;
 
-
-
-int main ( void ) 
+char entree[501];
+int main ( void )
 {
-   int size;
-   int q;
-   string abc = "abcdefghijklmnopqrstuvwxyz";
-   cin >> q;
-   size = 2 * q-1;
-   int x, y;
-   for(int i = 0; i< size; i++)
-   {
+	cin >> entree;
+	int nbLettres = (int) strlen(entree);
+	
+	int nbDoublons = 0;
+	while(nbDoublons != 0)
+	{
+		int iIn =0, iOut = 0;
+		
+	}
+	
    
-   for ( int j = 0; j < size; j++)
-   {
-      x = i;
-      y = j;
-	  x = ( x >= q ? size-1-x : x);
-      y = ( y >= q ? size-1-y : y);
-      
-      cout << abc[min(x, y)];
-   
-   }//end inner for
-   
-   cout << endl;
-   } //end outer for
-
-} //end main
-
+}
---
+++ Python/whileloop.py
@@ -0,0 +1,6 @@
+def is_par(var : bool) -> bool:
+    return bool(var%100+1)
+i = 1
+while a = is_par(i):
+    i+=1
+    print(a)
