eda360363676ed5680d0661b8af4017266065568
Elvis PTI <elvisavfc65@gmail.com>
2019-09-12 09:00:52 -0400

    Added operators overloading example

--- C++/c++45(operators).cpp
+++ C++/c++45(operators).cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 using namespace std;
+
+//We can overload operators as well, not only functions.
+/*
+Overloadable operators
++ - * / = < > += -= *= /= << >>
+<<= >>= == != <= >= ++ -- % & ^ ! |
+~ &= ^= |= & || %= [] () , ->* -> new
+delete new[] delete[]
+*/
 class Test
 {
    private:
@@ -12,11 +21,53 @@ class Test
        }
        void Display() { cout<<"Count: "<<count; }
 };
+
+//another possible syntax
+class Cvector
+{
+	public:
+		int x, y;
+		Cvector() {};
+		Cvector (int a, int  b) : x(a), y(b) {};
+		Cvector operator + (const Cvector&);
+};
+
+
+//Note the :: operator. This means that "+" is only defined inside Cvector class
+//and then it is called a member function overloading
+Cvector Cvector::operator + (const Cvector & param)
+{
+	Cvector temp;
+	temp.x = x + param.x;
+	temp.y = y + param.y;
+	return temp;
+ } 
+ 
+ //this is called a non-member function overloading
+ Cvector operator -(const Cvector & lhs, const Cvector&rhs)
+ {
+ 	Cvector temp;
+ 	temp.x = lhs.x - rhs.x;
+ 	temp.y = lhs.y - rhs.y;
+ 	return temp;
+ 	
+ }
 int main()
 {
     Test t;
     // this calls "function void operator ++()" function
     ++t;    
     t.Display();
+    cout << "\n";
+    
+    
+    
+    
+    Cvector foo(3, 1);
+    Cvector bar(1, 2);
+    Cvector result = foo + bar; //equivalent to a.operator+(b)
+    cout << result.x << ", " << result.y << "\n";
+    cout << (foo-bar).x << ", " << (foo- bar).y << "\n";
+    
     return 0;
 }
