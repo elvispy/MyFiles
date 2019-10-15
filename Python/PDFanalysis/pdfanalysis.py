f6e384c30b7c7c39c28da62d876e6d68f26ae1a2
Elvis PTI <elvisavfc65@gmail.com>
2019-10-15 12:36:07 -0300

    Added tdms example

--- Python/PDFanalysis/pdfanalysis.py
+++ Python/PDFanalysis/pdfanalysis.py
@@ -6,9 +6,29 @@ pdffile = open(os.getcwd() + "\\MEDALLERO 2019.pdf", "rb")
 
 pdfObject = PyPDF2.PdfFileReader(pdffile)
 
-pdfPage = pdfObject.getPage(0)
 
-noerror = True
+count = 1
 i = 0
-while noerror:
-    pdfPage = pdfObject.getpage(i).extractText()
+mydict = dict()
+while (count%34 and count < 371):
+    try:
+        pdfPage = pdfObject.getPage(i).extractText()
+
+    except Exception as e:
+        print(e)
+        break
+    content = [pdfPage.split("\n{}".format(count), maxsplit = 1)[-1]]
+    break
+    if i == 0 and count == 1:
+        content = content[:-2] + content[-1].split("PLATA", maxsplit = 1)
+        count + = 1
+        #trabajarle al primero
+    print(content)
+    while True:
+        count += 1
+        content = content[:-2] + content[-1].split("\n{}".format(count), maxsplit = 1)
+        
+
+    i = i+1    
+
+pdffile.close()
