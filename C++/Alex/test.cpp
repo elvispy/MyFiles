4561b9fedc2b9dfe1f5ca061d46af45a0287a351
Elvis PTI <elvisavfc65@gmail.com>
2019-10-17 07:12:33 -0300

    Added test.cpp for some franceioi problem

--- C++/Alex/test.cpp
+++ C++/Alex/test.cpp
@@ -1,52 +1,88 @@
 #include<iostream>
+#include<cctype>
 
 using namespace std;
 
+struct casillas
+{
+   int f;
+   int c;
+};
+
+bool valid(casillas a);
+
+casillas * crear(casillas b);
+
 int main()
 {
-   int N;
-   cin >> N;
-   int table[N][N];
-   for(int i = 0; i< N; i++)
-   {
-      for(int j = 0; j< N; j++)
-      {
-         cin >> table[i][j];
-      } //end inner for
-      cin.ignore();
-   }//end for
-   int aux = 0;
-   int aux2 = 0;
-   for(int l = 0; l < N; l++)
-   {
-      aux += table[l][l];
-      aux2 += table[N-1-l][l];
-   }//end for
-   if(aux != aux2)
-   {
-      cout << "no";
-      return 0;
-   }
+    char carre[8][8];
    
-   aux2 = 0;
-   int aux3 = 0;
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j< 8; j++)
+            cin >> carre[i][j];
+        cin.ignore();
+    }//end for
    
-   for(int i = 0; i < N; i++)
-   {
-      aux2 = 0;
-      aux3 = 0;
-      
-      for(int j = 0; j < N; j++)
-      {
-         aux2 += table[i][j];
-         aux3 += table[j][i];
-      }//end for
-      if(aux2 != aux || aux3 != aux)
-      {
-         cout << "no";
-         return 0;
-      }
-   }//end for
-   cout << "yes";
-   return 0;
+    for(int i = 0; i < 8; i++)
+    {
+        for(int j = 0; j< 8; j++)
+        {
+            if (carre[i][j] == 'C')
+            {
+            	casillas aux;
+            	aux.f = i;
+            	aux.c = j;
+            	casillas * creados = crear(aux);
+            	for(int l = 0; l<8;l++)
+            	{	
+            		if (valid(*(creados + l)))
+            		{
+            			casillas res = *(creados + l);
+            			if(islower(carre[res.f][res.c]))
+						{
+							cout << "yes";
+							return 0;
+						}//end if 	
+					}//end if	
+				}//end innermost for
+            }//end if
+        }//end for
+    }//end for
+    cout << "no";
+    return 0;
 }//end main
+
+
+casillas * crear(casillas b)
+{
+	static casillas r[8];
+	r[0].c = b.c + 2;
+	r[0].f = b.f + 1;
+	r[1].c = b.c + 2;
+	r[1].f = b.f - 1;
+	r[2].c = b.c - 2;
+	r[2].f = b.f + 1;
+	r[3].c = b.c - 2;
+	r[3].f = b.f - 1;
+	r[4].c = b.c + 1;
+	r[4].f = b.f + 2;
+	r[5].c = b.c + 1;
+	r[5].f = b.f - 2;
+	r[6].c = b.c - 1;
+	r[6].f = b.f + 2;
+	r[7].c = b.c - 1;
+	r[7].f = b.f - 2;
+	
+	return r;
+	
+	
+}//end function definition
+
+bool valid(casillas a)
+{
+    if( a.c < 0 || a.f < 0 || a.c>7 || a.f > 7)
+        return false;
+    return true;
+}//end function definition
+
