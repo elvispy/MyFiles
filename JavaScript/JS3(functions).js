27db5ff145080d9dafb57f6152a9e9d4bbc0c088
Elvis Aguero <elvis.aguero@pti.org.py>
2019-12-05 09:18:40 -0300

    Added function definitions to JS tutorial

---
+++ JavaScript/JS3(functions).js
@@ -0,0 +1,17 @@
+//We will see the two  ways of declaring functions
+
+//the first way is the way shared by  python and R, not anonymous.
+
+function MyFun(par1, par2 = "Defa"){
+  console.log("this is a " + par1 + par2 + "ult argument");
+}
+//as you can see, par2 is an optional parameter, with identical syntaxis
+//to python
+
+MyFun("hola");
+
+//the other way of defining functions is with the idea of anonymous functions
+
+const  mymul = function(row, col) {
+  return row * col;
+}
