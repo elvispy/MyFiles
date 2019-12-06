1aa10a22d8d50a559b6ea82a27c820c3e0bcd320
elvispy <elvisavfc65@gmail.com>
2019-12-06 10:05:17 -0300

    Added JS examples

--- JavaScript/JS5(Arrays1).js
+++ JavaScript/JS5(Arrays1).js
@@ -1,1 +1,22 @@
 //JavaScript Arrays
+
+//Arrays in JS are quite similar to Arrays in Python.
+
+let myarr = ['hola', 2, [2, null, undefined]]; //multiple data types are allowed
+
+console.log(myarr[2][1]); //indexing is the same
+//we can index a string value also
+var cmamo = 'asdf;lkjj';
+console.log(cmamo[4]);
+
+myarr[1] = 42; //reassignment is the same
+
+//some methods
+
+//.pop() will delete last value and return it
+//.push(val) will add values separated by commas at the end of the Array
+//.indexOf(val) will return the index of the value
+//.splice(pos, nb) will remove nb elements from the array starting at pos
+// and return them
+//.shit() will remoove the first element
+//.unshift(val) will add an element to the first part of the list
---
+++ JavaScript/JS6(loops).js
@@ -0,0 +1,24 @@
+//JavaScript
+//normal for loops are identical to the for loops in c++
+
+for(let myvar = 0; myvar < 12; myvar++)
+{
+  console.log(myvar);
+}
+
+//we also have list comprehension inside for loopss
+
+
+//the while loops are identical, also
+let myal = 1;
+while(myal > -10){
+  myal--;
+  console.log(myal);
+
+}
+
+//also the old friend do while expression
+let i = 0;
+do{
+  i++
+}while ( i < 12);
