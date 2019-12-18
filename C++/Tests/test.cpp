c55ff0a973e7382a95e037a3196a8fdb8d7dd0be
Elvis Aguero <elvis.aguero@pti.org.py>
2019-12-18 10:21:27 -0300

    Began coloraciones material

--- C++/Tests/test.cpp
+++ C++/Tests/test.cpp
@@ -1,28 +1,30 @@
 #include<bits/stdc++.h>
-
+#include<string>
 using namespace std;
 
-
 int main(){
-	cin.sync_with_stdio(false);
-	cin.tie(0);
-	int nb[1000] = {};
-	int dates[1000] = {};
-	int idx1 = 0;
-	int idx2 = 0;
-	int P;
-	cin >> P;
-	for(int i = 0; i<P; i++)
-	{
-		int num, dat;
-		cin >> num >> dat;
-		if(num > 0)
-		{
-			myPile = put(myPile, num, dat);
-		}else{
-			myPile = quit(myPile, -num);
-		}//end if
-	}//end for
-	cout << myPile.dates[myPile.idx1];
-	return 0;
-}
+   int N;
+   cin >> N;
+   cin.ignore();
+   string mot;
+   getline(cin, mot);
+   int v1 = 0;
+
+   for(char L: mot){
+   	  //cout << L << "\n";
+      if(L == '('){
+         v1++;
+      }else if(L == ')'){
+         v1--;
+      }//end if
+      if(v1<0){
+         cout << "0";
+         return 0;
+      }//end if
+      
+      cout << v1 << "\n";
+   }//end for
+   
+   cout << "1";
+   return 0;
+}//end main
