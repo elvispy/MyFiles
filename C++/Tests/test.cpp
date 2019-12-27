9d97442325caad600f58c0c74f8e7a4c5355a26a
Elvis Aguero <elvis.aguero@pti.org.py>
2019-12-27 15:17:23 -0300

    Added some problems to the coloracion material

--- C++/Tests/test.cpp
+++ C++/Tests/test.cpp
@@ -3,28 +3,10 @@
 using namespace std;
 
 int main(){
-   int N;
-   cin >> N;
-   cin.ignore();
-   string mot;
-   getline(cin, mot);
-   int v1 = 0;
-
-   for(char L: mot){
-   	  //cout << L << "\n";
-      if(L == '('){
-         v1++;
-      }else if(L == ')'){
-         v1--;
-      }//end if
-      if(v1<0){
-         cout << "0";
-         return 0;
-      }//end if
-      
-      cout << v1 << "\n";
-   }//end for
+   string lol = "alskdj";
    
-   cout << "1";
+   lol = lol + lol;
+   lol.replace(lol.begin(), lol.end(), "")
+   cout << lol;
    return 0;
 }//end main
